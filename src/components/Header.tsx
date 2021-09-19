74aa0b1d43e72e34999a3f89c1cdee39ecda3ecd
Michael Hermansen <michael.p.hermansen@gmail.com>
2021-09-20 00:56:56 +0200

    ...

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,23 @@
 import Link from 'next/link';
-import { SearchIcon } from '@heroicons/react/solid';
+import { SearchIcon, XIcon } from '@heroicons/react/solid';
+import { useState } from 'react';
 
 const Header = () => {
+	const [visibleSearch, setVisibleSearch] = useState(false);
+
 	return (
 		<header className='flex justify-between items-center p-3 border-b border-gray-100 bg-white'>
 			<Link href={'/filmer'}>
 				<a className='font-bold p-1'>Filmsøk</a>
 			</Link>
-			<Link href='/search'>
-				<a className='p-2 bg-gray-100 rounded-full'>
-					<SearchIcon className='h-4 w-4' />
-				</a>
-			</Link>
+			<button
+				onClick={() => setVisibleSearch(s => !s)}
+				className='p-2 bg-gray-100 rounded-full'
+				aria-label='Søk'
+			>
+				{visibleSearch && <SearchIcon className='h-4 w-4' />}
+				{!visibleSearch && <XIcon className='h-4 w-4' />}
+			</button>
 		</header>
 	);
 };
