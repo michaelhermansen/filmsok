836285a3075c469c9568c9f4f421f2b7a1413e66
Michael Hermansen <michael.p.hermansen@gmail.com>
2021-09-20 00:56:49 +0200

    funksjonell button component

--- src/components/shared/Button.tsx
+++ src/components/shared/Button.tsx
@@ -1,11 +1,15 @@
 interface ButtonProps {
 	children: any;
+	onClick: () => void;
 	Icon?: (props: React.ComponentProps<'svg'>) => JSX.Element;
 }
 
-const Button: React.FC<ButtonProps> = ({ children, Icon }) => {
+const Button: React.FC<ButtonProps> = ({ children, onClick, Icon }) => {
 	return (
-		<button className='px-5 py-2 bg-gray-200 font-medium rounded-lg flex gap-2 items-center'>
+		<button
+			onClick={onClick}
+			className='px-5 py-2 bg-gray-200 font-medium rounded-lg flex gap-2 items-center'
+		>
 			{children}
 			{Icon && <Icon className='w-4 h-4' />}
 		</button>
