5fa939c70fd4f83485af0f1abb4a7e456befb1c0
Michael Hermansen <michael.p.hermansen@gmail.com>
2021-09-20 19:17:41 +0200

    placeholder-bilde

--- src/components/MediaItem.tsx
+++ src/components/MediaItem.tsx
@@ -1,8 +1,10 @@
 import { LoadingContext } from '../lib/loadingContext';
 import { Result } from '../types/Response';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
+import getRatingColor from '../lib/getRatingColor';
 import Image from 'next/image';
 import Link from 'next/link';
+import translateGenreId from '../lib/translateGenreId';
 
 interface MediaItemProps {
 	media: Result;
@@ -12,10 +14,14 @@ const MediaItem: React.FC<MediaItemProps> = ({ media }) => {
 	const { loading } = useContext(LoadingContext);
 	const mediaDate = media.release_date || media.first_air_date || '';
 	const mediaYear = mediaDate.substring(0, 4);
-	const mediaPath = media.title ? 'filmer' : 'serier';
+	const mediaType = media.title ? 'filmer' : 'serier';
+	const ratingColor = getRatingColor(media.vote_average);
+	const mainGenre = translateGenreId(media.genre_ids[0], mediaType);
+
+	const [imgError, setImgError] = useState(false);
 
 	return (
-		<Link href={`/${mediaPath}/${media.id}`}>
+		<Link href={`/${mediaType}/${media.id}`}>
 			<a
 				className={`p-2 flex gap-4 items-center rounded-lg hover:bg-white hover:shadow-xl transition-all duration-300 ${
 					loading && 'opacity-10 pointer-events-none'
@@ -23,7 +29,12 @@ const MediaItem: React.FC<MediaItemProps> = ({ media }) => {
 			>
 				<div className='bg-gray-300 h-40 w-28 rounded-md flex-shrink-0 relative overflow-hidden'>
 					<Image
-						src={`https://image.tmdb.org/t/p/w200/${media.poster_path}`}
+						onError={() => setImgError(true)}
+						src={
+							!imgError
+								? `https://image.tmdb.org/t/p/w200/${media.poster_path}`
+								: '/assets/placeholder.svg'
+						}
 						width={250}
 						height={360}
 						objectFit='cover'
@@ -32,8 +43,16 @@ const MediaItem: React.FC<MediaItemProps> = ({ media }) => {
 				</div>
 				<div className='pr-2'>
 					<h2 className='mb-2 font-semibold'>{media.title || media.name}</h2>
-					<p>{mediaYear}</p>
-					<p className='mt-8'>{media.vote_average.toFixed(1)}</p>
+					<p className='text-sm'>
+						{mediaYear} {mediaYear && mainGenre ? '·' : ''} {mainGenre}
+					</p>
+					{media.vote_average > 0 && (
+						<div className='mt-8 flex gap-2 items-center'>
+							<p>{media.vote_average.toFixed(1)}</p>
+							<div className={'h-1 w-1 rounded-full' + ratingColor}></div>
+						</div>
+					)}
+					{!media.vote_average && <p className='mt-8'>N/A</p>}
 				</div>
 			</a>
 		</Link>
